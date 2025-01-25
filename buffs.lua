6efd843718191eb1d40ddf219a6902a0d9327637
Joshua Powers <jp@joshuapowers.com>
2025-01-24 20:41:57 -0600

    fix: overlapping spell ids, composure/perpetuance detection

--- buffs.lua
+++ buffs.lua
@@ -11,6 +11,7 @@ function buffs:new(params)
 
   -- internal variables
   instance._buffs            = {} -- keyed on buff id
+  instance._buff_order       = {} -- sorted buffs
 
   -- if we are currently hovering on a buff, it is this one
   instance._hovering_on_buff = {}
@@ -165,7 +166,40 @@ function buffs:update_buffs(buffs)
   local cols = math.floor(max_width / (self._buff_width + buff_spacing)) -- Number of buffs per row
   local rows = 0                                                         -- Track row count dynamically
 
-  -- Remove buffs that are no longer active first
+  -- **Step 1: Determine order of buffs**
+  self._buff_order = {} -- Maintain a sorted order of keys
+  if buffs then
+    for buff_key, buff_data in pairs(buffs) do
+      table.insert(self._buff_order, {
+        key = buff_key,
+        category = buff_data.category,
+        end_time = buff_data.end_time
+      })
+    end
+
+    -- Sort by category priority first, then by remaining time (longest-lasting buffs first)
+    table.sort(self._buff_order, function(a, b)
+      -- Define category priorities
+      local category_priority = {
+        ["dispelable buff"] = 1,
+        ["buff"] = 1
+      }
+
+      -- Get the priorities of the categories
+      local a_priority = category_priority[a.category] or 2 -- Default priority for other categories
+      local b_priority = category_priority[b.category] or 2
+
+      -- First, sort by category priority (lower numbers = higher priority)
+      if a_priority ~= b_priority then
+        return a_priority < b_priority
+      end
+
+      -- If categories are the same priority, sort by remaining time (longest-lasting first)
+      return a.end_time > b.end_time
+    end)
+  end
+
+  -- **Step 2: Remove inactive buffs**
   for buff_key, buff_data in pairs(self._buffs) do
     if not (buffs and buffs[buff_key]) then
       -- Buff no longer exists, remove it
@@ -182,155 +216,161 @@ function buffs:update_buffs(buffs)
     end
   end
 
-  -- Process incoming buffs and update positions
+  -- **Step 3: Process and render buffs**
   if buffs then
     local i = 0
-    for buff_key, buff in pairs(buffs) do
-      i = i + 1
+    for _, sorted_entry in ipairs(self._buff_order) do
+      local buff_key = sorted_entry.key
+      local buff = buffs[buff_key]
 
-      local new_x, new_y, updated_rows = calculate_buff_position(i, cols, self._buff_width, self._buff_height,
-        buff_spacing,
-        self._x, self._y)
+      -- just because its in the sorted order, doesn't mean it wasn't removed in step 2
+      if buff then
+        i = i + 1
 
-      -- Update row count
-      if updated_rows > rows then
-        rows = updated_rows
-      end
+        local new_x, new_y, updated_rows = calculate_buff_position(i, cols, self._buff_width, self._buff_height,
+          buff_spacing,
+          self._x, self._y)
 
-      if not self._buffs[buff_key] then
-        -- if an ability is a certain category, we give it a background to make it
-        -- stand out more for curing it.
-
-        local target_data = windower.ffxi.get_mob_by_id(buff.target_id)
-        local is_friendly = target_data.in_party or target_data.in_alliance or (not target_data.is_npc)
-        local new_buff_bg = nil
-        local is_creating_background = false
-        local bg_color = { 0, 0, 0 }
-
-        if is_friendly then
-          if buff.category == "sleep" then
-            is_creating_background = true
-            bg_color = { 0, 0, 0 }
-          elseif buff.category == "poison" then
-            is_creating_background = true
-            bg_color = { 26, 209, 3 }
-          elseif buff.category == "paralyze" then
-            is_creating_background = true
-            bg_color = { 255, 216, 0 }
-          elseif buff.category == "blind" then
-            is_creating_background = true
-            bg_color = { 128, 128, 128 }
-          elseif buff.category == "silence" then
-            is_creating_background = true
-            bg_color = { 102, 255, 183 }
-          elseif buff.category == "petrify" then
-            is_creating_background = true
-            bg_color = { 112, 78, 0 }
-          elseif buff.category == "disease" then
-            is_creating_background = true
-            bg_color = { 187, 131, 226 }
-          elseif buff.category == "curse" then
-            is_creating_background = true
-            bg_color = { 83, 0, 226 }
-          elseif buff.category == "uncurable" then
-            is_creating_background = true
-            bg_color = { 228, 0, 0 }
-          elseif buff.category == "erase" then
-            is_creating_background = true
-            bg_color = { 0, 182, 255 }
-          end
-        else
-          if buff.category == "dispelable buff" then
-            is_creating_background = true
-            bg_color = { 226, 62, 189 }
-          end
+        -- Update row count
+        if updated_rows > rows then
+          rows = updated_rows
         end
 
-        if is_creating_background then
-          new_buff_bg = images.new({
+        if not self._buffs[buff_key] then
+          -- if an ability is a certain category, we give it a background to make it
+          -- stand out more for curing it.
+
+          local target_data = windower.ffxi.get_mob_by_id(buff.target_id)
+          local is_friendly = target_data.in_party or target_data.in_alliance or (not target_data.is_npc)
+          local new_buff_bg = nil
+          local is_creating_background = false
+          local bg_color = { 0, 0, 0 }
+
+          if is_friendly then
+            if buff.category == "sleep" then
+              is_creating_background = true
+              bg_color = { 0, 0, 0 }
+            elseif buff.category == "poison" then
+              is_creating_background = true
+              bg_color = { 26, 209, 3 }
+            elseif buff.category == "paralyze" then
+              is_creating_background = true
+              bg_color = { 255, 216, 0 }
+            elseif buff.category == "blind" then
+              is_creating_background = true
+              bg_color = { 128, 128, 128 }
+            elseif buff.category == "silence" then
+              is_creating_background = true
+              bg_color = { 102, 255, 183 }
+            elseif buff.category == "petrify" then
+              is_creating_background = true
+              bg_color = { 112, 78, 0 }
+            elseif buff.category == "disease" then
+              is_creating_background = true
+              bg_color = { 187, 131, 226 }
+            elseif buff.category == "curse" then
+              is_creating_background = true
+              bg_color = { 83, 0, 226 }
+            elseif buff.category == "uncurable" then
+              is_creating_background = true
+              bg_color = { 228, 0, 0 }
+            elseif buff.category == "erase" then
+              is_creating_background = true
+              bg_color = { 0, 182, 255 }
+            end
+          else
+            if buff.category == "dispelable buff" then
+              is_creating_background = true
+              bg_color = { 226, 62, 189 }
+            end
+          end
+
+          if is_creating_background then
+            new_buff_bg = images.new({
+              flags = {
+                draggable = false
+              }
+            })
+
+            new_buff_bg:size(self._buff_width, self._buff_height)
+            new_buff_bg:path(windower.addon_path .. 'icons/bg.png')
+            new_buff_bg:repeat_xy(1, 1)
+            new_buff_bg:pos(new_x, new_y)
+            new_buff_bg:draggable(false)
+            new_buff_bg:alpha(255)
+            new_buff_bg:color(unpack(bg_color))
+            new_buff_bg:fit(false)
+          end
+
+          local new_buff_img = images.new({
             flags = {
               draggable = false
             }
           })
 
-          new_buff_bg:size(self._buff_width, self._buff_height)
-          new_buff_bg:path(windower.addon_path .. 'icons/bg.png')
-          new_buff_bg:repeat_xy(1, 1)
-          new_buff_bg:pos(new_x, new_y)
-          new_buff_bg:draggable(false)
-          new_buff_bg:alpha(255)
-          new_buff_bg:color(unpack(bg_color))
-          new_buff_bg:fit(false)
-        end
-
-        local new_buff_img = images.new({
-          flags = {
-            draggable = false
-          }
-        })
-
-        new_buff_img:size(self._buff_width - 2, self._buff_height - 2)
-        new_buff_img:path(windower.addon_path .. 'icons/' .. buff.buff_id .. '.bmp')
-        new_buff_img:fit(false)
-        new_buff_img:repeat_xy(1, 1)
-        new_buff_img:pos(new_x + 1, new_y + 1)
-        new_buff_img:draggable(false)
-        new_buff_img:alpha(255)
+          new_buff_img:size(self._buff_width - 2, self._buff_height - 2)
+          new_buff_img:path(windower.addon_path .. 'icons/' .. buff.buff_id .. '.bmp')
+          new_buff_img:fit(false)
+          new_buff_img:repeat_xy(1, 1)
+          new_buff_img:pos(new_x + 1, new_y + 1)
+          new_buff_img:draggable(false)
+          new_buff_img:alpha(255)
 
 
-        if self._isVisible then
-          if new_buff_bg then
-            new_buff_bg:show()
-          end
-          new_buff_img:show()
-        else
-          if new_buff_bg then
-            new_buff_bg:hide()
-          end
-          new_buff_img:hide()
-        end
-
-        self._buffs[buff_key] = {
-          background = new_buff_bg,
-          image = new_buff_img,
-          remaining_time = buff.end_time - os.clock(),
-          originating_spell = buff.originating_spell,
-          buff_name = res.buffs[buff.buff_id].en,
-          buff_category = buff.category,
-          current_x = new_x, -- Track initial position to optimize when pos gets updated
-          current_y = new_y  -- Track initial position to optimize when pos gets updated
-        }
-        changed = true
-      else
-        -- Update existing buff
-        self._buffs[buff_key].remaining_time = buff.end_time - os.clock()
-
-        -- the originating spell can change for certain skills
-        if self._buffs[buff_key].originating_spell ~= buff.originating_spell then
-          self._buffs[buff_key].originating_spell = buff.originating_spell
-        end
-
-        -- Check if the buff is near expiration
-        if buff.end_time - os.clock() <= near_expiration_threshold then
-          -- Blink effect: alternate opacity or color
-          if blink_phase == 0 then
-            self._buffs[buff_key].image:alpha(255) -- Fully visible
+          if self._isVisible then
+            if new_buff_bg then
+              new_buff_bg:show()
+            end
+            new_buff_img:show()
           else
-            self._buffs[buff_key].image:alpha(128) -- Semi-transparent
+            if new_buff_bg then
+              new_buff_bg:hide()
+            end
+            new_buff_img:hide()
           end
-        else
-          -- Reset to fully visible if not near expiration
-          self._buffs[buff_key].image:alpha(255)
-        end
 
-        -- Only update the position if it is actually changing
-        if self._buffs[buff_key].current_x ~= new_x or self._buffs[buff_key].current_y ~= new_y then
-          if self._buffs[buff_key].background then
-            self._buffs[buff_key].background:pos(new_x, new_y)  -- Update position
+          self._buffs[buff_key] = {
+            background = new_buff_bg,
+            image = new_buff_img,
+            remaining_time = buff.end_time - os.clock(),
+            originating_spell = buff.originating_spell,
+            buff_name = res.buffs[buff.buff_id].en,
+            buff_category = buff.category,
+            current_x = new_x, -- Track initial position to optimize when pos gets updated
+            current_y = new_y  -- Track initial position to optimize when pos gets updated
+          }
+          changed = true
+        else
+          -- Update existing buff
+          self._buffs[buff_key].remaining_time = buff.end_time - os.clock()
+
+          -- the originating spell can change for certain skills
+          if self._buffs[buff_key].originating_spell ~= buff.originating_spell then
+            self._buffs[buff_key].originating_spell = buff.originating_spell
           end
-          self._buffs[buff_key].image:pos(new_x + 1, new_y + 1) -- Update position
-          self._buffs[buff_key].current_x = new_x               -- Store new position
-          self._buffs[buff_key].current_y = new_y
+
+          -- Check if the buff is near expiration
+          if buff.end_time - os.clock() <= near_expiration_threshold then
+            -- Blink effect: alternate opacity or color
+            if blink_phase == 0 then
+              self._buffs[buff_key].image:alpha(255) -- Fully visible
+            else
+              self._buffs[buff_key].image:alpha(128) -- Semi-transparent
+            end
+          else
+            -- Reset to fully visible if not near expiration
+            self._buffs[buff_key].image:alpha(255)
+          end
+
+          -- Only update the position if it is actually changing
+          if self._buffs[buff_key].current_x ~= new_x or self._buffs[buff_key].current_y ~= new_y then
+            if self._buffs[buff_key].background then
+              self._buffs[buff_key].background:pos(new_x, new_y)  -- Update position
+            end
+            self._buffs[buff_key].image:pos(new_x + 1, new_y + 1) -- Update position
+            self._buffs[buff_key].current_x = new_x               -- Store new position
+            self._buffs[buff_key].current_y = new_y
+          end
         end
       end
     end
